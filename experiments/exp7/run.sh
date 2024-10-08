1490fbde847c5cedee504c7edf14fddcd52e6b89
DJ Passey <djpasseyjr@gmail.com>
2024-10-08 13:20:07 -0400

    Updated folder locations for experiment 7.

--- experiments/exp7/run.sh
+++ experiments/exp7/run.sh
@@ -4,10 +4,6 @@ module purge
 module load anaconda
 conda activate interfere_exp5
 
-pip uninstall interfere -y
-pip install git+https://www.github.com/djpasseyjr/interfere.git@6b600cafefbbc5771efeecc817da5d50fe8e9ca9
-pip install git+https://www.github.com/djpasseyjr/interfere_experiments.git
-
-python /nas/longleaf/home/djpassey/interfere_experiments/experiments/exp5/main.py
+python /nas/longleaf/home/djpassey/interfere_experiments/experiments/exp7/main.py
 
 
--- experiments/exp7/slurm_submit.sh
+++ experiments/exp7/slurm_submit.sh
@@ -2,9 +2,9 @@
 
 # Expected number of hours to run one simulation (always overestimate so that slurm doesnt kill the sim)
 HOURS_PER_SIM=120
-GIGS=16
+GIGS=32
 
 # Arguments for sbatch. Sets the appropriate time limit and directory
 FLAGS="--ntasks=1 --mem=${GIGS}G  --cpus-per-task=1 --time=$HOURS_PER_SIM:00:00"
 # Total number of jobs
-sbatch $FLAGS /nas/longleaf/home/djpassey/interfere_experiments/experiments/exp5/run.sh
+sbatch $FLAGS /nas/longleaf/home/djpassey/interfere_experiments/experiments/exp7/run.sh
