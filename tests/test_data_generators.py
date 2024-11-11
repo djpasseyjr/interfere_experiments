422f613302ddf0b7256d3765a376726ecb9e5637
DJ Passey <djpasseyjr@gmail.com>
2024-11-11 15:57:18 -0500

    Updated tests to match DataGenerator renaming.

---
+++ tests/test_data_generators.py
@@ -0,0 +1,20 @@
+from typing import Type
+
+import pytest
+import interfere_experiments as ie
+
+
+@pytest.mark.parametrize("data_gen_type", ie.data_generators.ALL_MODELS)
+class TestDataGenerators:
+
+    def test_generate(
+        self,
+        data_gen_type: Type[ie.data_generators.DataGenerator]
+    ):
+        dg = data_gen_type()
+        data = dg.generate_data(50, 50)
+        assert data.interv_states.shape == (50, dg.initial_condition.shape[1]), (
+            "Incorrect forecast shape generated for {model}"
+        )
+
+
